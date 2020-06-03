3eedc723ca1d075639cf0fdde304a542330ee11e
Yakov Kozlov <yakov.kozlov@alertlogic.com>
2020-06-03 13:36:22 +0300

    Allow to disable auto cast

--- include/decimal.hrl
+++ include/decimal.hrl
@@ -27,29 +27,36 @@
 
 -define(to_decimal(X), decimal:to_decimal(X, ?d_context)).
 -define(to_decimal(B,E), decimal:to_decimal(B,E, ?d_context)).
--define(to_binary(X), decimal:to_binary(?to_decimal(X),#{pretty=>?d_pretty})).
+
+-ifdef(d_no_autocast).
+-define(d_cast(X), X).
+-else.
+-define(d_cast(X), ?to_decimal(X)).
+-endif.
+
+-define(to_binary(X), decimal:to_binary(?d_cast(X),#{pretty=>?d_pretty})).
 -define(to_string(X), binary_to_list(?to_binary(X))).
 -define(to_float(X), binary_to_float(?to_binary(X))).
 
 -define(d_zero,{0,0}).
--define(add(X,Y),decimal:add(?to_decimal(X),?to_decimal(Y))).
--define(sub(X,Y),decimal:sub(?to_decimal(X),?to_decimal(Y))).
--define(mult(X,Y),decimal:mult(?to_decimal(X),?to_decimal(Y))).
--define(divide(X,Y),decimal:divide(?to_decimal(X),?to_decimal(Y),?d_context)).
--define(sqrt(X),decimal:sqrt(?to_decimal(X),?d_context)).
--define(abs(X),decimal:abs(?to_decimal(X))).
--define(neg(X),decimal:minus(?to_decimal(X))).
--define(sign(X),case element(1,?to_decimal(X))<0 of true -> -1; _ -> 1 end).
--define(exponent(X),element(2, ?to_decimal(X))).
--define(is_zero(X),decimal:is_zero(?to_decimal(X))).
+-define(add(X,Y),decimal:add(?d_cast(X),?d_cast(Y))).
+-define(sub(X,Y),decimal:sub(?d_cast(X),?d_cast(Y))).
+-define(mult(X,Y),decimal:mult(?d_cast(X),?d_cast(Y))).
+-define(divide(X,Y),decimal:divide(?d_cast(X),?d_cast(Y),?d_context)).
+-define(sqrt(X),decimal:sqrt(?d_cast(X),?d_context)).
+-define(abs(X),decimal:abs(?d_cast(X))).
+-define(neg(X),decimal:minus(?d_cast(X))).
+-define(sign(X),case element(1,?d_cast(X))<0 of true -> -1; _ -> 1 end).
+-define(exponent(X),element(2, ?d_cast(X))).
+-define(is_zero(X),decimal:is_zero(?d_cast(X))).
 -define(is_signed(X),case ?sign(X) of -1 -> true; _ -> false end).
--define(cmp(X,Y),decimal:cmp(?to_decimal(X),?to_decimal(Y), ?d_context)).
+-define(cmp(X,Y),decimal:cmp(?d_cast(X),?d_cast(Y), ?d_context)).
 -define(le(X,Y),(?cmp(X,Y) =< 0)).
 -define(lt(X,Y),(?cmp(X,Y) < 0)).
 -define(ge(X,Y),(?cmp(X,Y) >= 0)).
 -define(gt(X,Y),(?cmp(X,Y) > 0)).
 -define(max(X,Y),case ?cmp(X,Y) of 1->X;_->Y end).
 -define(min(X,Y),case ?cmp(X,Y) of -1->X;_->Y end).
--define(reduce(X),decimal:reduce(?to_decimal(X))).
+-define(reduce(X),decimal:reduce(?d_cast(X))).
 
 -endif.
